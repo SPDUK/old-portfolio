e3b1d0f394aec81a16f38071513bd5d5698cbfdf
spduk <spdevuk@gmail.com>
2019-03-20 14:28:18 +0000

    feat: customize project routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@
 
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :projects, except: [:show]
+  resources :blogs
+
+  get 'project/:id', to: 'projects#show', as: 'project_show'
   get '/about-me', to: 'pages#about'
   get '/contact', to: 'pages#contact'
-
-  resources :blogs
-  resources :projects
 end
