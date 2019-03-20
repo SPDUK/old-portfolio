e6167b04b86b078e8ac6e211bbe8cba1b6577501
spduk <spdevuk@gmail.com>
2019-03-20 13:40:02 +0000

    feat: add basic CRUD for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,59 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def create
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: 'project was successfully created.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to @project, notice: 'project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :subtitle, :body)
+  end
 end
