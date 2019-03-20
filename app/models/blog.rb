ef73ccd86b0816e9b7fc9f5af4b443715f10cf83
spduk <spdevuk@gmail.com>
2019-03-20 16:01:53 +0000

    feat: add status enum to manage draft state for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
+  # handles permission for checking if a blog is visible to the public
+  enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
