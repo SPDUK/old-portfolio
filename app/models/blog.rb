7dc00c456f3f864dfa53f6ce159ec92309b5695a
spduk <spdevuk@gmail.com>
2019-03-21 10:54:44 +0000

    feat: impliment validations for blogs, skills and project items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,9 @@
 class Blog < ApplicationRecord
   # handles permission for checking if a blog is visible to the public
   enum status: { draft: 0, published: 1 }
+
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :body
 end
