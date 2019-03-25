9869747fcf7021b3c4647163f35adcca0dff15fc
spduk <spdevuk@gmail.com>
2019-03-25 13:11:05 +0000

    feat: completed virtual attrs for user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # makes sure the name was supplied
+  validates_presence_of :name
+
+  # virtual attrs
+  def first_name
+    # self is the specific User (eg: User.last.first_name)
+    self.name.split.first
+  end
+  def last_name
+    self.name.split.last
+  end
 end
