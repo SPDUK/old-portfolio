ca51fc98bfcf9210433ae4ce37e3f9a80e6b7f2e
spduk <spdevuk@gmail.com>
2019-03-25 14:57:36 +0000

    refactor: moved set_source to controllers/concerns

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
   # custom path for devise
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
   root to: "pages#home"
+
   resources :projects, except: [:show]
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-
   get "projects/rails", to: "projects#rails"
   get "project/:id", to: "projects#show", as: "project_show"
+
   get "/about-me", to: "pages#about"
   get "/contact", to: "pages#contact"
 end
