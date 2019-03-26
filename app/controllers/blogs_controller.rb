5df9dd8416c9aba2a879f9d88f90490fab55b978
spduk <spdevuk@gmail.com>
2019-03-26 08:31:57 +0000

    feat: added navbar and default header/seo content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,11 +7,15 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "My blogs!"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
-  def show; end
+  def show
+    @page_title = @blog.title
+    @seo_keywords << @blog.title
+   end
 
   # GET /blogs/new
   def new
---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: false
+
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "SP | Portfolio"
+    @seo_keywords = "spdevuk spduk portfolio react ruby rails elixir javascript nodejs uk england cambridge "
+  end
+end
