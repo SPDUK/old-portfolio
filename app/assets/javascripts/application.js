aacad1bce337b9e99eb9234f7f50939641926bb7
spduk <spdevuk@gmail.com>
2019-03-27 20:05:57 +0000

    feat: added webgl background and some placeholder options for each path

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,62 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require three.min.js
+//= require waves.min.js
+
+const waveOptions = {
+  '/': {
+    el: '#site',
+    color: '#0e1012',
+    shininess: 61.0,
+    waveHeight: 12.5,
+    waveSpeed: 0.65,
+    zoom: 1
+  },
+  '/edit': {
+    el: '#site',
+    color: '#0F0B0C',
+    shininess: 32.0,
+    waveHeight: 24.5,
+    waveSpeed: 0.8,
+    zoom: 0.9
+  },
+  '/login': {
+    el: '#site',
+    color: '#020406',
+    shininess: 32.0,
+    waveHeight: 24.5,
+    waveSpeed: 0.8,
+    zoom: 0.9
+  },
+  '/register': {
+    el: '#site',
+    color: '#020406',
+    shininess: 32.0,
+    waveHeight: 24.5,
+    waveSpeed: 0.8,
+    zoom: 0.9
+  },
+  '/contact': {
+    el: '#site',
+    color: '#020406',
+    shininess: 32.0,
+    waveHeight: 24.5,
+    waveSpeed: 0.8,
+    zoom: 1.2
+  },
+  '/about-me': {
+    el: '#site',
+    color: '#020406',
+    shininess: 32.0,
+    waveHeight: 24.5,
+    waveSpeed: 0.8,
+    zoom: 0.6
+  }
+};
+document.addEventListener('turbolinks:load', function(event) {
+  const url = new URL(event.data.url);
+  VANTA.WAVES({
+    ...waveOptions[url.pathname]
+  });
+});
