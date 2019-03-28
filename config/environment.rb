809fe8811cfa9aad546c36966914a50d6f8f309a
spduk <spdevuk@gmail.com>
2019-03-28 10:56:30 +0000

    feat: add register styles for form

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,16 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+# changes error tag provided by actionview to add a bootstrap is-invalid class instead
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  class_attr_index = html_tag.index 'class="'
+
+  if class_attr_index
+    html_tag.insert class_attr_index + 7, "is-invalid "
+  else
+    html_tag.insert html_tag.index(">"), ' class="is-invalid"'
+  end
+end
