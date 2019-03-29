e04bc500e5bf29c9f06797df66025d41fd869d77
spduk <spdevuk@gmail.com>
2019-03-29 11:46:39 +0000

    refactor: move nav links into a nav_helper link generator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 module ApplicationHelper
   def login_helper(style = "")
@@ -15,5 +15,25 @@ module ApplicationHelper
       greeting = "Thanks for visiting me from #{session[:source]}"
       content_tag(:p, greeting, class: "source-greeting")
     end
+  end
+
+  def nav_helper(style, tag_type)
+    nav_items = [
+    { title: "Home", url: root_path },
+    { title: "About Me", url: about_me_path },
+    { title: "Contact", url: contact_path },
+    { title: "My Blog", url: blogs_path },
+    { title: "Projects", url: projects_path }
+    ]
+    nav_links = ""
+    nav_items.each do |link|
+      nav_links << "<#{tag_type}><a href=#{link[:url]} class='#{style} #{active? link[:url]}'>#{link[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+
+  def active?(path)
+    "active" if current_page? path
   end
 end
