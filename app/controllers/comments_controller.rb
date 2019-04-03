8ffade28ed54b413c304e3870137728e752c27b4
spduk <spdevuk@gmail.com>
2019-04-03 17:59:21 +0100

    feat: add initial form and partial call to display blog comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
+  # grab the current user and build a comment (automatically associates the comment to the user)
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
