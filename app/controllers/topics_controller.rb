4727aa3d0c2d9aba4c746a8e2b1862ee4bbc8007
spduk <spdevuk@gmail.com>
2019-04-06 17:32:18 +0100

    feat: add topic routes to see blogs related to topics, and a view to list all topics

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class TopicsController < ApplicationController
+  layout "blog"
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    if logged_in?(:site_admin)
+      @blogs = @topic.blogs.recent.page(params[:page]).per(5)
+    else
+      @blogs = @topic.blogs.recent.published.page(params[:page]).per(5).order(created_at: :desc)
+    end
+    @page_title = "My blogs!"
+  end
+end
