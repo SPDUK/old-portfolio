36160c0118c80954243e9c6931358b45921ac425
spduk <spdevuk@gmail.com>
2019-04-08 22:59:11 +0100

    fix: fix @blog being nil for show when not logged in

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[edit update destroy toggle_status]
+  before_action :set_blog, only: %i[show edit update destroy toggle_status]
   before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   # user authorization
   access all: [:show, :index],
