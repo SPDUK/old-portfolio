ad624e61cbc65d5e1be0155c2f74c71037b36bbd
SPDUK <spdevuk@gmail.com>
2019-05-10 09:17:58 +0100

    feat: use enable/disable of stylesheets to toggle theme

--- app/assets/javascripts/navbars/functions/setDarkTheme.js
+++ app/assets/javascripts/navbars/functions/setDarkTheme.js
@@ -1,6 +1,10 @@
 // adds the bootstrap dark theme, removes the light theme, animates the toggler
-function setDarkTheme() {
-  Cookies.set('theme', 'dark');
+
+// append runs synchronously, so we await the function finishing and only add/remove
+// the stylesheets when they're finished downloading
+async function downloadDarkCSS() {
+  // todo: add loading spinner or something to not flash the screen during load
+  console.log('loading dark theme......');
   $('head').append(
     '<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha384-w+8Gqjk9Cuo6XH9HKHG5t5I1VR4YBNdPt/29vwgfZR485eoEJZ8rJRbm3TR32P6k" crossorigin="anonymous" id="bootstrap-dark" />'
   );
@@ -10,8 +14,21 @@ function setDarkTheme() {
     type: 'text/css',
     href: 'https://res.cloudinary.com/dmjolhdaq/raw/upload/v1555203029/Portfolio/dracula.css'
   }).appendTo('head');
+}
+
+async function setDarkTheme() {
+  Cookies.set('theme', 'dark');
+
+  if (!$('#bootstrap-dark').length) {
+    await downloadDarkCSS();
+    console.log('done!');
+  }
+
+  // enable dark theme
+  $('#bootstrap-dark').removeAttr('disabled');
+  $('#syntax-dark').removeAttr('disabled');
+  // disable light theme
+  $('#bootstrap-light').attr('disabled', 'true');
 
   $('.theme-toggle').addClass('toggle-on');
-  $('#bootstrap-light').remove();
-  $('#syntax-light').remove();
 }
--- app/assets/javascripts/navbars/functions/setLightTheme.js
+++ app/assets/javascripts/navbars/functions/setLightTheme.js
@@ -1,7 +1,13 @@
 // removes the dark theme to set the default light one
 function setLightTheme() {
   Cookies.set('theme', 'light');
+
+  // enable the light theme
+  $('#bootstrap-light').removeAttr('disabled');
+
+  // remove the dark theme
+  $('#bootstrap-dark').attr('disabled', 'true');
+  $('#syntax-dark').attr('disabled', 'true');
+
   $('.theme-toggle').removeClass('toggle-on');
-  $('#bootstrap-dark').remove();
-  $('#syntax-dark').remove();
 }
