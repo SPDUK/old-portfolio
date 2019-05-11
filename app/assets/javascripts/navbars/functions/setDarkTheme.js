dab00f00e099432b5cfefb4d88e72dc66d76f70f
SPDUK <spdevuk@gmail.com>
2019-05-11 03:19:23 +0100

    feat: refactor theme toggle to display loading spinner on download

--- app/assets/javascripts/navbars/functions/setDarkTheme.js
+++ app/assets/javascripts/navbars/functions/setDarkTheme.js
@@ -1,27 +1,39 @@
+//= require "helpers/load"
 // adds the bootstrap dark theme, removes the light theme, animates the toggler
 
 // append runs synchronously, so we await the function finishing and only add/remove
 // the stylesheets when they're finished downloading
-async function downloadDarkCSS() {
-  // todo: add loading spinner or something to not flash the screen during load
-  console.log('loading dark theme......');
-  $('head').append(
-    '<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha384-w+8Gqjk9Cuo6XH9HKHG5t5I1VR4YBNdPt/29vwgfZR485eoEJZ8rJRbm3TR32P6k" crossorigin="anonymous" id="bootstrap-dark" />'
-  );
-  $('<link>', {
-    rel: 'stylesheet',
-    id: 'syntax-dark',
-    type: 'text/css',
-    href: 'https://res.cloudinary.com/dmjolhdaq/raw/upload/v1555203029/Portfolio/dracula.css'
-  }).appendTo('head');
+
+function setCSSAtribs(styles) {
+  styles.setAttribute('type', 'text/css');
+  styles.setAttribute('rel', 'stylesheet');
+}
+function downloadDarkCSS() {
+  $('#loading').addClass('show');
+
+  const syntaxDark =
+    'https://res.cloudinary.com/dmjolhdaq/raw/upload/v1555203029/Portfolio/dracula.css';
+  const bootstrapDark =
+    'https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css';
+
+  // load all the css files, then add an id and stop loading when they've finished loading
+  Promise.all([load.css(syntaxDark), load.css(bootstrapDark)])
+    .then(() => {
+      // add the IDs to the links we just added
+      document.querySelector(`link[href='${bootstrapDark}']`).id = 'bootstrap-dark';
+      document.querySelector(`link[href='${syntaxDark}']`).id = 'syntax-dark';
+
+      $('#loading').removeClass('show');
+    })
+    // reload page if it fails to load
+    .catch(() => window.location.reload());
 }
 
 async function setDarkTheme() {
   Cookies.set('theme', 'dark');
 
   if (!$('#bootstrap-dark').length) {
-    await downloadDarkCSS();
-    console.log('done!');
+    downloadDarkCSS();
   }
 
   // enable dark theme
