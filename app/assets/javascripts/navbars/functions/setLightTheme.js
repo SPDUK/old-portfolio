0383c650aa3cc551f61f397200127139863ecff8
SPDUK <spdevuk@gmail.com>
2019-05-11 07:41:24 +0100

    fix: remove flash of unstyled text while changing themes

--- app/assets/javascripts/navbars/functions/setLightTheme.js
+++ app/assets/javascripts/navbars/functions/setLightTheme.js
@@ -5,9 +5,12 @@ function setLightTheme() {
   // enable the light theme
   $('#bootstrap-light').removeAttr('disabled');
 
-  // remove the dark theme
-  $('#bootstrap-dark').attr('disabled', 'true');
-  $('#syntax-dark').attr('disabled', 'true');
+  // remove the dark theme after a delay to avoid flash of unstyled text
+  // (only takes a few ms to read from cache b ut it happens sometimes anyway)
+  setTimeout(() => {
+    $('#bootstrap-dark').attr('disabled', 'true');
+    $('#syntax-dark').attr('disabled', 'true');
+  }, 100);
 
   $('.theme-toggle').removeClass('toggle-on');
 }
