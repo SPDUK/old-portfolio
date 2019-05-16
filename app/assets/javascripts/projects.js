498fdca58acd3009d395d07526972462c99dea04
SPDUK <spdevuk@gmail.com>
2019-05-16 10:54:36 +0100

    feat: animations for fading in text and button for carousel

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -28,8 +28,56 @@ async function afterSort() {
   });
 }
 
+function animateCarouselIn(evt = false) {
+  anime
+    .timeline()
+    .add({
+      easing: 'easeOutQuad',
+      translateX: [-30, 0],
+      targets: '.carousel-caption h5',
+      opacity: [0, 1],
+      duration: 350
+    })
+    .add({
+      easing: 'easeOutQuad',
+      targets: '.carousel-caption .btn',
+      translateX: [-30, -0],
+      opacity: [0, 1],
+      duration: 350
+    });
+}
+
+function animateCarouselOut(evt = false) {
+  anime
+    .timeline()
+    .add({
+      easing: 'easeOutQuad',
+      targets: '.carousel-caption h5',
+      translateX: [0, -30],
+      opacity: [1, 0],
+      duration: 200
+    })
+    .add({
+      easing: 'easeOutQuad',
+      targets: '.carousel-caption .btn',
+      translateX: [0, -30],
+      opacity: [1, 0],
+      duration: 200
+    });
+}
 $(document).on('turbolinks:load', () => {
   loadingSpinner();
+
+  $('.carousel').carousel({
+    interval: 5000
+  });
+  animateCarouselIn();
+
+  // when the slide begins
+  $('.carousel').on('slide.bs.carousel', animateCarouselOut);
+
+  // after carousel has finished sliding
+  $('.carousel').on('slid.bs.carousel', animateCarouselIn);
 
   const projects = document.getElementById('sortable');
   if (projects) {
