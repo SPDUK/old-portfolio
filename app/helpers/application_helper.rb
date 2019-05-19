564ea6751e6c1ed44902b2d0543f8e56946182ed
SPDUK <spdevuk@gmail.com>
2019-05-19 20:35:39 +0100

    fix: make navbar links minimal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,14 +17,6 @@ module ApplicationHelper
     markdown.render(text).html_safe
   end
 
-  def login_helper(style = "")
-    if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      (link_to "Login", new_user_session_path, class: style)
-    else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
-    end
-  end
 
 
   # wrap each link in a tag type, optionally pass in a class
@@ -34,7 +26,8 @@ module ApplicationHelper
     nav_items = [
     { title: "HOME", url: root_path },
     { title: "BLOG", url: blogs_path },
-    { title: "PROJECTS", url: projects_path }
+    { title: "PROJECTS", url: projects_path },
+    { title: "GITHUB", url: "https://github.com/SPDUK" }
     ]
     nav_links = ""
     nav_items.each do |link|
