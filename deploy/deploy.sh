cc6c8e689a80f0b4292256f0b89cff54149ee731
SPDUK <spdevuk@gmail.com>
2019-05-24 17:43:14 +0100

    feat: deploy scripts for git hooks

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,10 @@
+# stop the currently running server
+docker-compose down
+
 prod="docker-compose.prod.yml"
 # build the containers
 docker-compose -f $prod build 
-# create and migrate the DB
+# create and migrate the DB to the latest versions
 docker-compose run -f $prod app rake db:create RAILS_ENV=production
 docker-compose run -f $prod app rake db:migrate RAILS_ENV=production
 
---
+++ deploy/setup.sh
@@ -0,0 +1,2 @@
+cp deploy/post-receive .git/hooks
+chmod +x .git/hooks/post-receive
