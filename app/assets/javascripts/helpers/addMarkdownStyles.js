cedbb6fea389793c67be5a54b5db24e32f9d8ab8
SPDUK <spdevuk@gmail.com>
2019-06-03 22:08:49 +0100

    feat: add copy button function and notification

--- app/assets/javascripts/helpers/addMarkdownStyles.js
+++ app/assets/javascripts/helpers/addMarkdownStyles.js
@@ -1,7 +1,32 @@
+// when clicked copy any of the code inside to the clipboard
+async function handleCopyClick(evt) {
+  // get all of the pre divs (code blocks), remove any extra divs, such as the copy button
+  const codeBlocks = $(evt.target.parentElement)
+    .children()
+    .filter('pre');
+
+  // get the text of the codeBlocks
+  const text = codeBlocks.text();
+
+  // copy all of the text to the clipboard
+  await navigator.clipboard.writeText(text);
+
+  // notify of success
+  $.gritter.add({
+    title: 'Code copied to clipboard! 👍',
+    text: ' ',
+    time: 3000
+  });
+}
+
 function addMarkdownStyles() {
+  // add borders to each blockquote
   $('blockquote').each((_idx, el) => $(el).addClass('border-left border-dark '));
-  // todo: add copy link to each div.highlight
+
   $('div.highlight').each((_idx, el) => {
+    // add a copy button to each code block
     $(el).append($('<div class="copy">Copy</div>'));
+    // add an event to run when the button is clicked to copy the code to clipboard
+    $(el).click(handleCopyClick);
   });
 }
