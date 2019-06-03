3fc126cf2685e87099f110895ba1ded2b44cc73e
SPDUK <spdevuk@gmail.com>
2019-06-03 21:38:57 +0100

    feat: add copy buttons to all markdown code

--- app/assets/javascripts/helpers/addMarkdownStyles.js
+++ app/assets/javascripts/helpers/addMarkdownStyles.js
@@ -1,4 +1,7 @@
 function addMarkdownStyles() {
   $('blockquote').each((_idx, el) => $(el).addClass('border-left border-dark '));
   // todo: add copy link to each div.highlight
+  $('div.highlight').each((_idx, el) => {
+    $(el).append($('<div class="copy">Copy</div>'));
+  });
 }
