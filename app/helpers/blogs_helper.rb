5d82837ed2b912a0f33b216cfbfe31d7ccd0b36f
SPDUK <spdevuk@gmail.com>
2019-06-04 03:03:58 +0100

    chore: remove unused code block

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 module BlogsHelper
-  def gravatar_helper(user)
-    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 40
-  end
-
-
   def blog_status_icon(blog)
     blog.draft? ? "mr-3 fa-upload" : "mr-3 fa-check"
   end
